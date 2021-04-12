e3a35d5c7f285eb1cbeae3cebb40faba851ec8b1
BButner <butner.beau@gmail.com>
2021-04-12 16:54:50 -0500

    Fixes SectionSplash name not being responsive

--- components/index/SectionSplash.tsx
+++ components/index/SectionSplash.tsx
@@ -38,7 +38,7 @@ export const SectionSplash: FunctionComponent = () => {
       <SectionSplashSquares />
       <div className="z-40 space-y-4 md:space-y-8">
         <img className="w-32 h-32 md:w-52 md:h-52 rounded-full m-auto" src="/img/avatar_small.png" alt="Beau Butner Avatar" />
-        <motion.div className="flex justify-center text-center text-8xl" variants={titleVariants}>
+        <motion.div className="flex justify-center text-center text-4xl md:text-8xl" variants={titleVariants}>
           <div className={styles['emoji-wave']} aria-hidden="true">👋</div>
           <h1><span aria-hidden="true">, </span>I'm Beau Butner</h1>
         </motion.div>
