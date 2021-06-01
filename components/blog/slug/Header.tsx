3b8667553a74bf7fe93d0a045602eabfa7341e1a
BButner <butner.beau@gmail.com>
2021-06-01 16:28:27 -0500

    Header should link to /blog

--- components/blog/slug/Header.tsx
+++ components/blog/slug/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export const Header: FC = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight my-8 mx-2 md:mx-0">
-      <Link href="/">
+      <Link href="/blog">
         <a className="hover:underline">Beau Butner - Blog</a>
       </Link>
     </h2>
