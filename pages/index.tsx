444b58b0f61beef4d02d30b00386a6ee3983ee7f
BButner <butner.beau@gmail.com>
2021-06-17 12:03:24 -0500

    Index SEOv1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,38 @@
+import { NextSeo } from 'next-seo'
+import { useRouter } from 'next/router'
 import { FunctionComponent } from 'react'
 import { SectionSplash } from '../components/index/SectionSplash'
 import { Layout } from '../components/layout'
 
-const index: FunctionComponent = () => {
+const Index: FunctionComponent = () => {
+  const router = useRouter()
+
   return (
     <Layout pageTitle="Beau Butner">
+      <NextSeo
+        title="BButner - Blog"
+        description="My personal blog for tech."
+        openGraph={{
+          url: router.asPath,
+          type: 'website',
+          title: 'Beau Butner - Blog',
+          description: 'My personal blog for tech.',
+          locale: 'en_US',
+          images: [
+            {
+              url: '/img/avatar_small.png',
+              width: 1200,
+              height: 630,
+              alt: 'Beau Butner'
+            }
+          ],
+        }}
+        twitter={{
+          handle: '@BeauButner',
+          site: '@bbutner.com',
+          cardType: 'summary_large_image',
+        }}
+      />
       <SectionSplash />
     </Layout>
   )
