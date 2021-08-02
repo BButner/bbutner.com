5e8a114a751f3540ffeb10351f619c795fb543dc
BButner <beau.butner@siu.edu>
2021-08-02 12:03:12 -0500

    Job description change

--- components/about/AboutInfo.tsx
+++ components/about/AboutInfo.tsx
@@ -3,7 +3,7 @@ import { FunctionComponent } from 'react'
 export const AboutInfo: FunctionComponent = () => {
   return (
     <div className="text-2xl md:text-4xl">
-      <p>I am currently a full time Senior Systems Analyst working for a company that deploys, supports, and customizes mid-tier accounting software for companies that do anywhere between $5-$50 million dollars in yearly revenue.
+      <p>I am currently a Software Engineer for a company that manufactures flight simulation training devices, which over 300 training devices delivered in approximately 70 countries throughout the world. These flight simulators are used in all segments of the aviation industry, incuding prominent college aviation programs, such as Purdue University, Indiana State University, Embry-Riddle Aeronautical University, and more.
         My passion is my work, which means I put in as much effort as I can in order to produce quality and performant products, as well as providing the highest quality level of support, with quick and efficient turnaround times.
       </p>
     </div>
